7f857a3003b08a4a0a134354baca3a17cde27058
LaxExe <150862984+LaxExe@users.noreply.github.com>
2024-09-20 07:56:22 -0400

    Create helloWorldsAndStuff.java
    This is my first official Java program I learnt this about a year ago at the start of grade 10 around the 2nd week of September.

---
+++ helloWorldsAndStuff.java
@@ -0,0 +1,39 @@
+// helloWorldsAndStuff.java
+// Demonstrates various print methods and primitive data types this showcases what I learnt and was my first Java program over 20 lines!!!
+
+public class MyFirstProgram{ 
+    public static void main(String[] args) {
+      
+        // Basic print statements
+        System.out.print("Hello, World! ");
+        System.out.println("This is a single line.");
+
+        // variables String, int, char, double, boolean
+        String name = "John";
+        int age = 25;
+        char initial = 'J';
+        double pi = 3.14159;
+        boolean isAdult = age >= 18;
+
+        // Print variable values
+        System.out.println("Name: " + name + ", Age: " + age);
+        System.out.println("Initial: " + initial);
+        System.out.println("Pi: " + pi);
+        System.out.println("Is Adult? " + isAdult);
+
+        // Formatted output
+        System.out.printf("Formatted Pi: %.2f\n", pi);
+        System.out.printf("Name: %s, Age: %d\n", name, age);
+
+        // Additional primitive types I learnt!
+        byte smallNumber = 120;
+        short mediumNumber = 32000;
+        long largeNumber = 123456789L;
+        float decimalNumber = 5.75f;
+
+        System.out.println("Byte: " + smallNumber);
+        System.out.println("Short: " + mediumNumber);
+        System.out.println("Long: " + largeNumber);
+        System.out.println("Float: " + decimalNumber);
+    }
+}
