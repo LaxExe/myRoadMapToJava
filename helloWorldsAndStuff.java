efad006b71fa6895b3679c2b79e5f81497ebd632
LaxExe <150862984+LaxExe@users.noreply.github.com>
2024-09-20 07:59:08 -0400

    Update helloWorldsAndStuff.java
    Added a bit more comments.

--- helloWorldsAndStuff.java
+++ helloWorldsAndStuff.java
@@ -1,7 +1,7 @@
 // helloWorldsAndStuff.java
 // Demonstrates various print methods and primitive data types this showcases what I learnt and was my first Java program over 20 lines!!!
 
-public class MyFirstProgram{ 
+public class helloWorldsAndStuff{ 
     public static void main(String[] args) {
       
         // Basic print statements
@@ -15,13 +15,13 @@ public class MyFirstProgram{
         double pi = 3.14159;
         boolean isAdult = age >= 18;
 
-        // Print variable values
+        // Print variable values using the "+" sign and concatination
         System.out.println("Name: " + name + ", Age: " + age);
         System.out.println("Initial: " + initial);
         System.out.println("Pi: " + pi);
         System.out.println("Is Adult? " + isAdult);
 
-        // Formatted output
+        // Formatted output using printf
         System.out.printf("Formatted Pi: %.2f\n", pi);
         System.out.printf("Name: %s, Age: %d\n", name, age);
 
@@ -30,7 +30,8 @@ public class MyFirstProgram{
         short mediumNumber = 32000;
         long largeNumber = 123456789L;
         float decimalNumber = 5.75f;
-
+        
+        //Python would have made this same project with sooooo much less syntax :(
         System.out.println("Byte: " + smallNumber);
         System.out.println("Short: " + mediumNumber);
         System.out.println("Long: " + largeNumber);
